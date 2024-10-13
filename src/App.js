f21efa32b62b5a0648ea13ce9b42b59471ab9380
Nikhil Jadav <nikhil.jadhav3210@gmail.com>
2024-10-13 20:38:36 +0530

    state added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import './App.css';
-import Employee from './components/Employee';
+import "./App.css";
+import Employee from "./components/Employee";
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev');
   const showEmployeeBool = true;
   return (
     <div className="App">
-      {showEmployeeBool ?
+      {showEmployeeBool ? (
         <>
-          <Employee name="Nikhil" role="software"/>
-          <Employee name="jaiminee" role="farm"/>
-          <Employee name="jash" role="finance"/>
-          <Employee name="vishal"/>
-        </> 
-        :
-        <p>you do not have permission to see the employees.</p>}
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <Employee name="Nikhil" role="software" />
+          <Employee name="jash" role={role} />
+        </>
+      ) : (
+        <p>you do not have permission to see the employees.</p>
+      )}
     </div>
   );
 }
